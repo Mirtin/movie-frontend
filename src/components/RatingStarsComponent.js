9c08f5314b48e26a21b9b79b07d9a01a72e5f1ff
Mirtin <hack58if@gmail.com>
2024-05-19 21:58:28 +0200

    happy

--- src/components/RatingStarsComponent.js
+++ src/components/RatingStarsComponent.js
@@ -1,11 +1,11 @@
-import * as React from 'react';
+import { useEffect, useState } from 'react';
 import Rating from '@mui/material/Rating';
 import Box from '@mui/material/Box';
 import StarIcon from '@mui/icons-material/Star';
 
 const labels = {
-  0.5: 'Useless',
-  1: 'Useless+',
+  0.5: 'Bad',
+  1: 'Bad+',
   1.5: 'Poor',
   2: 'Poor+',
   2.5: 'Ok',
@@ -20,9 +20,19 @@ function getLabelText(value) {
   return `${value} Star${value !== 1 ? 's' : ''}, ${labels[value]}`;
 }
 
-const RatingStarsComponent = () => {
-  const [value, setValue] = React.useState(2);
-  const [hover, setHover] = React.useState(-1);
+function roundToNearestHalf(value) {
+  return Math.round(value * 2) / 2;
+}
+
+const RatingStarsComponent = ({averageRating}) => {
+  const [value, setValue] = useState(averageRating);
+  const [hover, setHover] = useState(-1);
+
+  useEffect(() => {
+    setValue(roundToNearestHalf(averageRating));
+  }, [averageRating]);
+
+
 
   return (
     <Box
--- src/pages/MoviePage.js
+++ src/pages/MoviePage.js
@@ -1,5 +1,5 @@
 import { useParams } from "react-router-dom";
-import { useEffect, useState} from 'react';
+import { useEffect, useState } from 'react';
 
 import { Box, Typography } from '@mui/material/';
 import axios from 'axios';
@@ -8,27 +8,38 @@ import HeaderComponent from '../components/HeaderComponent';
 import RatingStarsComponent from "../components/RatingStarsComponent";
 import TrailerPlayerComponent from "../components/TrailerPlayerComponent";
 
-
 const MoviePage = () => {
-
     const { movie_title } = useParams();
 
     const [data, setData] = useState({ id: 0, title: null, description: null, image: null, trailer: null });
+    const [averageRating, setAverageRating] = useState(null);
 
     const getMovieData = (movie_title) => {
         axios.get(`http://127.0.0.1:8000/api/movie/${movie_title}`)
-          .then(res => {
-            const responseData = res.data;
-            setData(responseData);
-          })
-          .catch(error => {
-            console.error('Error fetching movie data:', error);
-          });
-      };
-  
-      useEffect(() => {
+            .then(res => {
+                const responseData = res.data;
+                setData(responseData);
+            })
+            .catch(error => {
+                console.error('Error fetching movie data:', error);
+            });
+    };
+
+    const getAverageRating = (movie_title) => {
+        axios.get(`http://127.0.0.1:8000/api/average_rating/${movie_title}`)
+            .then(res => {
+                const res_rating = res.data.average_rating;
+                setAverageRating(res_rating);
+            })
+            .catch(error => {
+                console.error('Error fetching rating data:', error);
+            });
+    };
+
+    useEffect(() => {
+        getAverageRating(movie_title);
         getMovieData(movie_title);
-      }, [movie_title]);
+    }, [movie_title]);
 
 
     return (
@@ -39,28 +50,23 @@ const MoviePage = () => {
                 height: "60vh",
                 display: "flex",
                 justifyContent: "center",
+            }}>
+                <Box sx={{ height: "100%", width: '30%' }}>
+                    <img src={data.image} alt={data.title} style={{ borderRadius: "5%", height: "80%" }} />
+                    <RatingStarsComponent averageRating={averageRating} />
+                    <Typography variant='h3'>{data.title}</Typography>
+                </Box>
+                <Box sx={{
+                    height: "100%",
+                    width: '70%',
+                    display: "flex",
+                    justifyContent: "center"
                 }}>
-
-              <Box sx={{height: "100%", width: '30%'}}>  
-                <img src={data.image} alt={data.title} style={{borderRadius: "5%", height: "80%"}}/>
-                <RatingStarsComponent />
-                <Typography variant='h3'>{data.title}</Typography>
-              </Box>
-
-              <Box sx={{
-                height: "100%", 
-                width: '70%',
-                display: "flex",
-                justifyContent: "center" 
-                }}>
-              <TrailerPlayerComponent url={data.trailer} />
-              </Box>
-
+                    <TrailerPlayerComponent url={data.trailer} />
+                </Box>
             </Box>
-
-
-            <Box sx={{backgroundColor: "#E5DDC5", height: "27vh"}}>
-              <Typography variant='h4'>{data.description}</Typography>
+            <Box sx={{ backgroundColor: "#E5DDC5", height: "27vh" }}>
+                <Typography variant='h4'>{data.description}</Typography>
             </Box>
         </>
     );
